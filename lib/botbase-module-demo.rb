d41b297ce6e34e30dfa1a08147eb5b636732227b
Your Name <you@example.com>
2016-07-27 19:46:11 +0100

    1st commit

---
+++ lib/botbase-module-demo.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+
+# file: botbase-module-demo.rb
+
+# A service module used by the BotBase gem
+
+class BotBaseModuleDemo
+  
+  def initialize(x=nil)
+    
+    @phrases = {}
+    service_modules = nil
+    
+    a = if service_modules then
+    
+      service_modules
+
+    else
+      
+      @h = {
+          "^What's the time?" => "'The time is ' + Time.now.strftime(\"%-I:%M%p\")",
+          "hello?" => %q("hello #{sender}, how can I help you?" % sender),
+          'unknown' => "\"I'm sorry, I don't understand what you said.\""
+      }
+      [@h]
+
+    end
+    
+    a.each {|x|  @phrases.merge! x }  
+
+  end
+
+  def query(sender='user01', msg)
+    r = @phrases.detect {|k,v|  msg =~ /#{k}/i }
+
+    raw_msg = if r then
+      r.last 
+    else  
+      @h['unknown']      
+    end
+    
+    response = eval raw_msg   
+  end
+
+end
